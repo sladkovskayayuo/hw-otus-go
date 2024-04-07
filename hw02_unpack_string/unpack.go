180b4060967aba3ee2f834ae65734d183d5cd19f
sladkovskaya.yuo <sladkovskaya.yuo@gmail.com>
2024-04-07 23:11:23 +0300

    hw2 lint

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -10,40 +10,69 @@ var ErrInvalidString = errors.New("invalid string")
 func Unpack(str string) (string, error) {
 
 	if str == "" {
+
 		return "", nil
+
 	}
 
 	n := -1
+
 	var result strings.Builder
+
 	letter := rune(-1)
 
 	for i, char := range str {
+		//nolint:ifElseChain
 		if char > '9' || char < '0' {
+
 			if letter == -1 {
+
 				letter = char
+
 			} else {
+
 				result.WriteString(string(letter))
+
 				letter = char
+
 			}
+
 		} else if char >= '0' && char <= '9' {
+
 			if n == -1 && letter != -1 {
+
 				n = (int(char) - '0')
+
 			} else {
+
 				return "", ErrInvalidString
+
 			}
+
 		} else {
+
 			return "", ErrInvalidString
+
 		}
+
 		if n != -1 {
+
 			result.WriteString(strings.Repeat(string(letter), n))
+
 			n = -1
+
 			letter = -1
+
 		}
 
 		if i == len(str)-1 && letter != -1 {
+
 			result.WriteString(string(letter))
+
 		}
+
 	}
 
 	return result.String(), nil
+
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -8,57 +8,96 @@ import (
 )
 
 func TestUnpack(t *testing.T) {
+
 	tests := []struct {
-		input    string
+		input string
+
 		expected string
 	}{
+
 		{input: "a4bc2d5e", expected: "aaaabccddddde"},
+
 		{input: "abccd", expected: "abccd"},
+
 		{input: "", expected: ""},
+
 		{input: "aaa0b", expected: "aab"},
+
 		// uncomment if task with asterisk completed
+
 		// {input: `qwe\4\5`, expected: `qwe45`},
+
 		// {input: `qwe\45`, expected: `qwe44444`},
+
 		// {input: `qwe\\5`, expected: `qwe\\\\\`},
+
 		// {input: `qwe\\\3`, expected: `qwe\3`},
+
 	}
 
 	TestsGroupNoErrorRuner(tests, t)
+
 }
 
 func TestUnpackInvalidString(t *testing.T) {
+
 	invalidStrings := []string{"3abc", "45", "aaa10b"}
+
 	for _, tc := range invalidStrings {
+
 		tc := tc
+
 		t.Run(tc, func(t *testing.T) {
+
 			_, err := Unpack(tc)
+
 			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
+
 		})
+
 	}
+
 }
 
 func TestUnpackStringWithSpecialChar(t *testing.T) {
+
 	tests := []struct {
-		input    string
+		input string
+
 		expected string
 	}{
+
 		{input: "d\n5abc", expected: "d\n\n\n\n\nabc"},
+
 		{input: "d\t4", expected: "d\t\t\t\t"},
+
 		{input: "d\n", expected: "d\n"},
 	}
+
 	TestsGroupNoErrorRuner(tests, t)
+
 }
 
 func TestsGroupNoErrorRuner(tests []struct {
-	input    string
+	input string
+
 	expected string
 }, t *testing.T) {
+
 	for _, tc := range tests {
+
 		tc := tc
+
 		t.Run(tc.input, func(t *testing.T) {
+
 			result, err := Unpack(tc.input)
+
 			require.NoError(t, err)
+
 			require.Equal(t, tc.expected, result)
+
 		})
+
 	}
+
 }

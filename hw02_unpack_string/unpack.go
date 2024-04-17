9b8da4c0690ed313c6d01ae11d1a595743b2c187
sladkovskaya.yuo <sladkovskaya.yuo@gmail.com>
2024-04-17 17:07:13 +0300

    hw2 lint

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -8,11 +8,8 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(str string) (string, error) {
-
 	if str == "" {
-
 		return "", nil
-
 	}
 
 	n := -1
@@ -22,53 +19,34 @@ func Unpack(str string) (string, error) {
 	letter := rune(-1)
 
 	for _, char := range str {
-
 		if char > '9' || char < '0' {
-
 			if letter == -1 {
-
 				letter = char
-
 			} else {
-
 				result.WriteString(string(letter))
 
 				letter = char
-
 			}
-
 		} else {
-
 			if n == -1 && letter != -1 {
-
 				n = (int(char) - '0')
-
 			} else {
-
 				return "", ErrInvalidString
-
 			}
-
 		}
 
 		if n != -1 {
-
 			result.WriteString(strings.Repeat(string(letter), n))
 
 			n = -1
 
 			letter = -1
-
 		}
-
 	}
 
 	if letter != -1 {
-
 		result.WriteString(string(letter))
-
 	}
 
 	return result.String(), nil
-
 }

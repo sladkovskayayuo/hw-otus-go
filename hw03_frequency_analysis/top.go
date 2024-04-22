8d0b1d92e87e0ec60d6c3259f3f25cb5feeb4e2e
sladkovskaya.yuo <sladkovskaya.yuo@gmail.com>
2024-04-22 21:14:06 +0300

    hw3

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,45 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(str string) []string {
+	if str == "" {
+		return []string{}
+	}
+	collection := strings.Fields(str)
+	m := make(map[string]int)
+	for _, word := range collection {
+		m[word]++
+	}
+
+	type kv struct {
+		Key   string
+		Value int
+	}
+
+	var sm []kv
+	for k, v := range m {
+		sm = append(sm, kv{k, v})
+	}
+
+	sort.Slice(sm, func(i, j int) bool {
+		if sm[i].Value == sm[j].Value {
+			return sm[i].Key < sm[j].Key
+		}
+		return sm[i].Value > sm[j].Value
+	})
+
+	var keys []string
+	for i, v := range sm {
+		if i < 10 || sm[9].Value == sm[i].Value {
+			keys = append(keys, v.Key)
+		} else {
+			break
+		}
+	}
+
+	return keys[:10]
 }
